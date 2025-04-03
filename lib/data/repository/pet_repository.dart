c49e93cbf8588bf91e78bb09b31efc7642e71873
panicoli0 <pablonicoli21@gmail.com>
2025-04-03 20:44:21 -0300

    moved PetRepositoryImpl

--- lib/data/repository/pet_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pets_shop/domain/models/pet.dart';
-
-abstract class IPetRepository {
-  Future<List<Pet>> getPets();
-  Future<void> addPet(Pet pet);
-  Future<List<Pet>> searchPets(String query);
-  Future<List<Pet>> filterByBreed(String breed);
-}
