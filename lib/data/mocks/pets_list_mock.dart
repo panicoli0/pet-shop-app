ff6b21f8c8c6f2a12935b786c35bbac4d396bd13
panicoli0 <pablonicoli21@gmail.com>
2025-04-05 10:24:52 -0300

    created mock data

---
+++ lib/data/mocks/pets_list_mock.dart
@@ -0,0 +1,36 @@
+class PetsListMock {
+  static const List<Map<String, dynamic>> petsListJson = [
+    {
+      "id": 1,
+      "name": "Bella",
+      "age": 3,
+      "type": "Dog",
+      "breed": "Labrador",
+      "imageUrl": "https://example.com/images/bella.jpg",
+    },
+    {
+      "id": 2,
+      "name": "Max",
+      "age": 2,
+      "type": "Cat",
+      "breed": "Siamese",
+      "imageUrl": "https://example.com/images/max.jpg",
+    },
+    {
+      "id": 3,
+      "name": "Charlie",
+      "age": 4,
+      "type": "Dog",
+      "breed": "Beagle",
+      "imageUrl": "https://example.com/images/charlie.jpg",
+    },
+    {
+      "id": 4,
+      "name": "Molly",
+      "age": 1,
+      "type": "Cat",
+      "breed": "Persian",
+      "imageUrl": "https://example.com/images/molly.jpg",
+    },
+  ];
+}
