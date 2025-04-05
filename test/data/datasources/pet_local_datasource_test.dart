2d49017c400862cf07bd1fc5541d8ea0c7478987
panicoli0 <pablonicoli21@gmail.com>
2025-04-05 10:27:05 -0300

    updated test

--- test/data/datasources/pet_local_datasource_test.dart
+++ test/data/datasources/pet_local_datasource_test.dart
@@ -1,19 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:pets_shop/data/datasources/pet_local_datasource.dart';
+import 'package:pets_shop/data/datasources/pet_remote_datasource.dart';
 import 'package:pets_shop/data/repository/pet_repository_impl.dart';
 import 'package:pets_shop/domain/entities_DTOs/pet_entity.dart';
 
 class MockPetLocalDataSource extends Mock implements IPetLocalDataSource {}
 
+class MockPetRemoteDataSource extends Mock implements IPetRemoteDataSource {}
+
 void main() {
   late PetRepositoryImpl repository;
   late MockPetLocalDataSource mockLocalDataSource;
+  late MockPetRemoteDataSource mockPetRemoteDataSource;
 
   setUp(() {
     mockLocalDataSource = MockPetLocalDataSource();
+    mockPetRemoteDataSource = MockPetRemoteDataSource();
     repository = PetRepositoryImpl(
       localDataSource: mockLocalDataSource,
+      remoteDataSource: mockPetRemoteDataSource,
     );
   });
 
