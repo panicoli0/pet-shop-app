cc6834deec670ae0127461a113f3f7d59948b857
panicoli0 <pablonicoli21@gmail.com>
2025-04-05 11:43:20 -0300

    update repo interface

--- lib/domain/repository/pet_repository.dart
+++ lib/domain/repository/pet_repository.dart
@@ -1,8 +1,9 @@
+import 'package:pets_shop/data/models/pet_model.dart';
 import 'package:pets_shop/domain/entities_DTOs/pet_entity.dart';
 
 abstract class IPetRepository {
   Future<List<PetEntity>> getPets();
-  Future<void> addPet(PetEntity pet);
+  Future<void> addPet(PetModel pet);
   Future<List<PetEntity>> searchPets(String query);
   Future<List<PetEntity>> filterByBreed(String breed);
 }
