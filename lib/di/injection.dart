76c401df9ce33107298adb3635f445f0b07c6964
panicoli0 <pablonicoli21@gmail.com>
2025-04-05 10:26:56 -0300

    update injection

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,23 +1,36 @@
 import 'package:get_it/get_it.dart';
+import 'package:pets_shop/core/config/dio_config.dart';
 import 'package:pets_shop/data/datasources/pet_local_datasource.dart';
+import 'package:pets_shop/data/datasources/pet_remote_datasource.dart';
 import 'package:pets_shop/data/repository/pet_repository_impl.dart';
+import 'package:pets_shop/data/services/pets_service.dart';
 import 'package:pets_shop/domain/repository/pet_repository.dart';
 import 'package:pets_shop/domain/usecases/get_pets.dart';
 import 'package:pets_shop/domain/usecases/search_pets.dart';
 import 'package:pets_shop/presentation/bloc/pet_list_bloc.dart';
+import 'package:dio/dio.dart';
 
 final getIt = GetIt.instance;
 
 void initializeDependencies() {
+  // Network
+  getIt.registerLazySingleton<Dio>(() => DioConfig.createDio());
+  getIt.registerLazySingleton<PetService>(() => PetService(getIt()));
+
   // Data Sources
   getIt.registerLazySingleton<IPetLocalDataSource>(
     () => PetLocalDataSource(),
+  );
+
+  getIt.registerLazySingleton<IPetRemoteDataSource>(
+    () => PetRemoteDataSource(),
   );
 
   // Repositories
   getIt.registerLazySingleton<IPetRepository>(
     () => PetRepositoryImpl(
       localDataSource: getIt(),
+      remoteDataSource: getIt(),
     ),
   );
 
