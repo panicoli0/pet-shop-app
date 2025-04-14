83a9ab3a13b4ef6ad8177d9de35653f8aa5e3838
panicoli0 <pablonicoli21@gmail.com>
2025-04-14 17:41:18 -0300

    updated injection

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -2,17 +2,26 @@ import 'package:get_it/get_it.dart';
 import 'package:pets_shop/core/config/dio_config.dart';
 import 'package:pets_shop/data/datasources/pet_local_datasource.dart';
 import 'package:pets_shop/data/datasources/pet_remote_datasource.dart';
+import 'package:pets_shop/data/repository/cart_repository_impl.dart';
 import 'package:pets_shop/data/repository/pet_repository_impl.dart';
+import 'package:pets_shop/data/services/hive_service.dart';
 import 'package:pets_shop/data/services/pets_service.dart';
+import 'package:pets_shop/domain/repository/cart_repository.dart';
 import 'package:pets_shop/domain/repository/pet_repository.dart';
 import 'package:pets_shop/domain/usecases/get_pets.dart';
 import 'package:pets_shop/domain/usecases/search_pets.dart';
+import 'package:pets_shop/presentation/bloc/cart/bloc/cart_bloc.dart';
 import 'package:pets_shop/presentation/bloc/pet_list_bloc.dart';
 import 'package:dio/dio.dart';
 
 final getIt = GetIt.instance;
 
-void initializeDependencies() {
+Future<void> initializeDependencies() async {
+  // Initialize Hive
+  final hiveService = HiveService();
+  await hiveService.initializeHive();
+  getIt.registerSingleton<HiveService>(hiveService);
+
   // Network
   getIt.registerLazySingleton<Dio>(() => DioConfig.createDio());
   getIt.registerLazySingleton<PetService>(() => PetService(getIt()));
@@ -36,6 +45,10 @@ void initializeDependencies() {
     ),
   );
 
+  getIt.registerLazySingleton<ICartRepository>(
+    () => CartRepositoryImpl(getIt()),
+  );
+
   // Use cases
   getIt.registerLazySingleton(() => GetPetsUseCase(getIt()));
   getIt.registerLazySingleton(() => SearchPetsUseCase(getIt()));
@@ -46,5 +59,8 @@ void initializeDependencies() {
       getPetsUseCase: getIt(),
       searchPetsUseCase: getIt(),
     ),
+  );
+  getIt.registerFactory(
+    () => CartBloc(getIt<ICartRepository>()),
   );
 }
