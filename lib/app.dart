14ded90e4262929d785c832bd9c05e0bc1c9f815
panicoli0 <pablonicoli21@gmail.com>
2025-04-14 17:39:32 -0300

    refactor app

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:pets_shop/presentation/pages/cart_page.dart';
-import 'package:pets_shop/presentation/pages/pet_list_page.dart';
+import 'package:pets_shop/core/config/routes/pet_route_information_parser.dart';
+import 'package:pets_shop/core/config/routes/pet_router_delegate.dart';
 
-class PetShopApp extends StatelessWidget {
+class PetShopApp extends StatefulWidget {
   const PetShopApp({super.key});
 
   @override
+  State<PetShopApp> createState() => _PetShopAppState();
+}
+
+class _PetShopAppState extends State<PetShopApp> {
+  final _routerDelegate = PetRouterDelegate();
+  final _routeInformationParser = PetRouteInformationParser();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Pet Shop',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const PetListPage(),
-        '/cart': (context) => const CartPage(),
-      },
+      routerDelegate: _routerDelegate,
+      routeInformationParser: _routeInformationParser,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pets_shop/app.dart';
 import 'package:pets_shop/di/injection.dart';
-import 'package:pets_shop/presentation/bloc/cart/bloc/cart_bloc.dart';
-import 'package:pets_shop/presentation/bloc/pet_list_bloc.dart';
-import 'package:pets_shop/presentation/bloc/pet_list_event.dart';
 
-void main() {
-  initializeDependencies();
-
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => getIt<PetListBloc>()..add(LoadPets()),
-        ),
-        BlocProvider(
-          create: (context) => CartBloc(),
-        ),
-      ],
-      child: const PetShopApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
+  runApp(const PetShopApp());
 }
